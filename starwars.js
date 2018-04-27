391281c54b1b6a8e88fb87e859b84ad078f54820
Lauro Jacques <lauro.jacques@dtidigital.com.br>
2018-04-26 23:50:45 -0300

    Commit funcionando sem extras e armazenamento local

--- starwars.js
+++ starwars.js
@@ -3,3 +3,64 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+function obtemListaDeFilmes() {
+  $.ajax({
+    url: "https://swapi.co/api/films/",
+    method: 'GET',
+    success: function (resposta) {
+      ordenaEpisodios(resposta)
+      preencheListaDeFilmes(resposta);
+    }
+  });
+}
+
+function obtemFilme(urlDoFilme) {
+  $.ajax({
+    url: urlDoFilme,
+    method: 'GET',
+    success: function (resposta) {
+      atualizaTexto(resposta.opening_crawl);
+    }
+  });
+}
+
+function atualizaTexto(texto) {
+  let elementoTexto = document.querySelector('.container>.flow>.reading-animation');
+  elementoTexto.innerText = texto;
+
+}
+
+function preencheListaDeFilmes(listaDeFilmes) {
+  let navFilmes = document.querySelector('#movies');
+  let elementoUl = document.createElement('ul');
+
+  for (let i = 0; i < listaDeFilmes.count; i++) {
+    let novoLi = document.createElement('li');
+    let tituloDoFilme = listaDeFilmes.results[i].title;
+
+    let texto = 'Episode ' + converteParaRomano(listaDeFilmes.results[i].episode_id) + ': ' + tituloDoFilme;
+
+    novoLi.appendChild(document.createTextNode(texto));
+    novoLi.addEventListener('click', () => {
+      obtemFilme(listaDeFilmes.results[i].url);
+    });
+    elementoUl.appendChild(novoLi);
+  }
+
+  navFilmes.appendChild(elementoUl);
+}
+
+function converteParaRomano(numero) {
+  const listaRomana = [null, 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
+
+  return listaRomana[numero];
+}
+
+function ordenaEpisodios(listaDeFilmes) {
+  listaDeFilmes.results.sort(function (a, b) {
+    return a.episode_id - b.episode_id;
+  });
+}
+
+obtemListaDeFilmes();
